8ab890db8e7195f178eb658a52c7caa0475effb9
Stevão Rodrigues <162615972+Stevaoo@users.noreply.github.com>
2024-11-22 21:51:38 -0300

    Create Dados.java

---
+++ outer_banks/Dados.java
@@ -0,0 +1,21 @@
+package Outer_Banks;
+
+/**
+ *
+ * @author STEVAOSEVERORODRIGUE
+ */
+
+import java.util.Scanner;
+public class Dados {
+  public int rolarDados(Scanner scanner, int quantidade, int faces){
+      int resultadoTotal = 0;
+      for(int i = 1;  i <= quantidade; i++){
+          System.out.println("Pressione Enter para rolar o dado" + i + "de" + quantidade + "(d" + faces + ").");
+          scanner.nextLine();
+          int resultado = (int) (Math.random()* faces) + 1;
+          System.out.println("Você rolou: " + resultado);
+          resultadoTotal += resultado;
+      }
+      return resultadoTotal;
+  }
+}
